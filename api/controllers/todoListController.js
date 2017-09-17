0f8dae49c36214b55899b94bdc444f05b32fd45e
Bryan Cesar Aguilar Perez <Bryan@MacBook-Pro-de-Bryan.local>
2017-09-17 11:13:45 -0500

    SERVICIO POST

--- api/controllers/todoListController.js
+++ api/controllers/todoListController.js
@@ -3,7 +3,7 @@
 
 exports.list_all_tasks = function(req, res) {
 
-  var sql = "SELECT * FROM `ordenes`";
+  var sql = "SELECT * FROM `orden`";
 
   con.getconnection().query(sql, function (err, result, fields) {
     con.getconnection().end();
@@ -11,6 +11,21 @@ exports.list_all_tasks = function(req, res) {
       console.log(result);
       res.status(200)
       .json(result);
+  });
+
+};
+
+exports.insert = function(req, res) {
+   console.log(JSON.stringify(req.body));
+  //console.log(req.body.idorden + "bryan");
+  // console.log(req.request.body.toString() + "bryan");
+  var sql = 'INSERT INTO orden SET ?';
+  con.getconnection().query(sql, req.body, function (err, result) {
+    // con.getconnection().query(sql, req.body, function (err, result) {
+    con.getconnection().end();
+      if (err) throw err;
+      console.log(result);
+      res.send('user added to database with ID:');
   });
 
 };
--- api/routes/todoListRoutes.js
+++ api/routes/todoListRoutes.js
@@ -3,6 +3,14 @@ module.exports = function(app) {
   var todoList = require('../controllers/todoListController');
 
   // todoList Routes
-  app.route('/')
+var bodyParser = require('body-parser');
+// app.use(bodyParser);
+ app.use(bodyParser.json());
+ app.use(bodyParser.urlencoded({ extended: true }));
+
+
+  app.route('/GET')
     .get(todoList.list_all_tasks);
+  app.route('/POST')
+  .post(todoList.insert);
 };
